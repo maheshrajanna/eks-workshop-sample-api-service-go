8e4ad5ca41aef70aec64b6342add2e452cd4d0b5
Mahesh Rajanna <maheshraj2886@gmail.com>
2019-07-10 20:03:01 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World How to change the data??"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
